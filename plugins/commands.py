3ba7d16029d6c9a012fc13320d58f6e9920855f3
T E L E Z E N <94111048+TELEZEN@users.noreply.github.com>
2022-01-26 21:19:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -53,7 +53,7 @@ async def start(client, message):
             InlineKeyboardButton('Aʙᴏᴜᴛ Mᴇ', callback_data='about'),
             InlineKeyboardButton('Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ', url='https://t.me/Arjun_La_Lis_A')
             ],[
-            InlineKeyboardButton('FILTΞRS', show_alert='This option is not ready yet. I will message you whet it is released...')
+            InlineKeyboardButton('FILTΞRS', callback data='filtermessage')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
